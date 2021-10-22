36b4d9719f8485098ace948eb1bb8683782a1af9
dcwatson84 <dcwatson84@gmail.com>
2021-10-22 09:46:38 -0400

    system view

--- src/main/java/org/thshsh/crypt/web/view/SystemView.java
+++ src/main/java/org/thshsh/crypt/web/view/SystemView.java
@@ -3,10 +3,14 @@ package org.thshsh.crypt.web.view;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.thshsh.crypt.serv.PortfolioHistoryService;
 import org.thshsh.crypt.web.views.main.MainLayout;
+import org.thshsh.vaadin.entity.ConfirmDialogs;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -18,6 +22,9 @@ public class SystemView extends VerticalLayout {
 	
 	@Autowired
 	PortfolioHistoryService histService;
+	
+	@Autowired
+	ApplicationContext context;
 
 	@PostConstruct
 	public void postConstruct() {
@@ -27,6 +34,18 @@ public class SystemView extends VerticalLayout {
 		runHist.addClickListener(click -> {
 			histService.runHistoryJob();
 		});
+		
+		Button shutdown = new Button("Shutdown", VaadinIcon.POWER_OFF.create(), click -> {
+			ConfirmDialogs.yesNoDialog("Shutdown system?", () -> {
+				int exitCode = SpringApplication.exit(context, () -> 0);
+				System.exit(exitCode);
+			}).open();
+			;
+		});
+		
+		
+
+		add(shutdown);
 	}
 	
 }
