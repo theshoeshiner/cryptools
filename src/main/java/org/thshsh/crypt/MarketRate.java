6a34efd874a59813ae42c7ecbd5321c970ce9139
dcwatson84 <dcwatson84@gmail.com>
2022-01-24 17:51:01 -0500

    fields for historical prices

--- src/main/java/org/thshsh/crypt/MarketRate.java
+++ src/main/java/org/thshsh/crypt/MarketRate.java
@@ -1,6 +1,7 @@
 package org.thshsh.crypt;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.ZonedDateTime;
 
 import javax.persistence.Column;
@@ -17,20 +18,54 @@ public class MarketRate extends IdedEntity {
 
 	@Column(columnDefinition = "decimal")
 	BigDecimal rate;
+	
+	@Column(columnDefinition = "decimal")
+	public BigDecimal high;
+	
+	@Column(columnDefinition = "decimal")
+	public BigDecimal low;
+	
+	@Column(columnDefinition = "decimal")
+	public BigDecimal open;
+	
+	@Column(columnDefinition = "decimal")
+	public BigDecimal close;
 
 	@Column
 	ZonedDateTime timestamp;
+	
+	@Column
+	ZonedDateTime startTime;
+	
+	@Column
+	ZonedDateTime endTime;
 
 
 
 	public MarketRate() {}
 
+	public MarketRate(Currency currency, BigDecimal high,BigDecimal low,BigDecimal open,BigDecimal close, ZonedDateTime start, ZonedDateTime end) {
+		super();
+		this.currency = currency;
+		this.startTime = start;
+		this.endTime = end;
+		//timestamp is considered to be the exact middle
+		this.timestamp = start.plus(Duration.between(start, end).dividedBy(2));
+		this.high = high;
+		this.low = low;
+		this.open = open;
+		this.close = close;
+		this.rate = low.add(high).add(open).add(close).divide(BigDecimal.valueOf(4));
+	}
+	
 	public MarketRate(Currency currency, BigDecimal rate, ZonedDateTime timestamp) {
 		super();
 		this.currency = currency;
 		this.rate = rate;
 		this.timestamp = timestamp;
 	}
+	
+	
 
 	public Currency getCurrency() {
 		return currency;
@@ -58,9 +93,23 @@ public class MarketRate extends IdedEntity {
 		this.timestamp = timestamp;
 	}
 
+	/**
+	 * This means the price is a historical quote from a specific hour instead of a realtime quote
+	 */
+	public Boolean isRange() {
+		return startTime != null && endTime != null;
+	}
+	
 	@Override
 	public String toString() {
-		return "[id=" + id + ", currency=" + currency + ", rate=" + rate + ", timestamp=" + timestamp + "]";
+		return "[id=" + id + 
+				", timestamp=" + timestamp + 
+				", currency=" + currency + 
+				", rate=" + rate +
+				", high=" + high + ", low=" + low
+				+ ", open=" + open + ", close=" + close + 
+				", startTime=" + startTime
+				+ ", endTime=" + endTime + "]";
 	}
 
 
