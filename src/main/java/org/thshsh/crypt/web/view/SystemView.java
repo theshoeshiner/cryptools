cf108b17985f3bf1552eae2dc39ab5c6722785fb
dcwatson84 <dcwatson84@gmail.com>
2022-01-24 17:55:54 -0500

    updates

--- src/main/java/org/thshsh/crypt/web/view/SystemView.java
+++ src/main/java/org/thshsh/crypt/web/view/SystemView.java
@@ -2,12 +2,18 @@ package org.thshsh.crypt.web.view;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.thshsh.crypt.Access;
 import org.thshsh.crypt.Feature;
 import org.thshsh.crypt.serv.PortfolioHistoryService;
+import org.thshsh.crypt.tax.CryptoReport;
 import org.thshsh.crypt.web.security.SecuredByFeatureAccess;
 import org.thshsh.crypt.web.views.main.MainLayout;
 import org.thshsh.vaadin.entity.ConfirmDialogs;
@@ -24,14 +30,28 @@ import com.vaadin.flow.router.Route;
 @SecuredByFeatureAccess(feature=Feature.System,access=Access.Super)
 public class SystemView extends VerticalLayout {
 	
+	public static final Logger LOGGER = LoggerFactory.getLogger(SystemView.class);
+	
+	
+	
 	@Autowired
 	PortfolioHistoryService histService;
 	
 	@Autowired
 	ApplicationContext context;
 
+	@Autowired
+	AsyncTaskExecutor executor;
+	
+	@Autowired
+	PlatformTransactionManager transactionManager;
+	
+	TransactionTemplate template;
+	
 	@PostConstruct
 	public void postConstruct() {
+		
+		template = new TransactionTemplate(transactionManager);
 		
 		Button runHist = new Button("Run History Job");
 		add(runHist);
