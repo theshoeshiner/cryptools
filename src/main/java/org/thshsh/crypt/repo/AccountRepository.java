5ff074c11ede7f776ebbbae5b21ae2ea9fd29d57
dcwatson84 <dcwatson84@gmail.com>
2022-02-11 08:40:10 -0500

    cleaned up

--- src/main/java/org/thshsh/crypt/repo/AccountRepository.java
+++ src/main/java/org/thshsh/crypt/repo/AccountRepository.java
@@ -1,12 +1,7 @@
 package org.thshsh.crypt.repo;
 
-import java.util.List;
-
 import org.thshsh.crypt.Account;
-import org.thshsh.crypt.User;
 
 public interface AccountRepository extends BaseRepository<Account, Long> {
-
-	//public List<Account> findByUser(User user);
 
 }
