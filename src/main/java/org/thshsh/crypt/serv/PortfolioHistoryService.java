e1839de097b16530b036b9250e8d4fe13a6e473a
dcwatson84 <dcwatson84@gmail.com>
2022-08-16 15:40:36 -0400

    force history runs

--- src/main/java/org/thshsh/crypt/serv/PortfolioHistoryService.java
+++ src/main/java/org/thshsh/crypt/serv/PortfolioHistoryService.java
@@ -1,10 +1,13 @@
 package org.thshsh.crypt.serv;
 
+import org.quartz.JobDetail;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.Trigger;
 import org.quartz.TriggerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.thshsh.crypt.Portfolio;
@@ -12,6 +15,8 @@ import org.thshsh.crypt.job.HistoryJob;
 
 @Service
 public class PortfolioHistoryService {
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(PortfolioHistoryService.class);
 	
 	@Autowired
 	Scheduler scheduler;
@@ -21,11 +26,15 @@ public class PortfolioHistoryService {
 	}
 	
 	public void runHistoryJob(Portfolio entity) {
+		
+		LOGGER.info("runHistoryJob: {}",entity);
+		
 		try {
-			scheduler.getJobDetail(JobKey.jobKey(""));
+			JobDetail jd = scheduler.getJobDetail(JobKey.jobKey("history-job"));
 			Trigger t = TriggerBuilder.newTrigger().forJob("history-job").startNow().build();
 			if(entity != null) {
 				t.getJobDataMap().put(HistoryJob.PORTFOLIO_ID_PROP, entity.getId().toString());
+				t.getJobDataMap().put(HistoryJob.FORCE_PROP,true);
 			}
 			//t.getJobDataMap().put(HistoryJob.FORCE_PROP, true);
 			scheduler.scheduleJob(t);
